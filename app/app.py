5cd884ba272d04fd9ed4ba611eebbb38c6b7b6f5
nikmosi <i@nikmosi.ru>
2025-03-10 19:57:40 +0700

    add encode endpoint

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
+from io import BytesIO
 from pathlib import Path
 
-from litestar import Litestar, get
+import qrcode
+from litestar import Litestar, Request, Response, get, post
 from litestar.contrib.jinja import JinjaTemplateEngine
 from litestar.openapi.config import OpenAPIConfig
 from litestar.openapi.plugins import SwaggerRenderPlugin
@@ -13,13 +15,32 @@ def home() -> Template:
     return Template(template_name="index.html")
 
 
+@post("/encode")
+async def encode(request: Request) -> Response:
+    form_data = await request.form()
+    text = form_data.get("data", "")
+
+    if not text:
+        return Response(
+            content="No data provided", media_type="text/plain", status_code=400
+        )
+
+    # Генерируем QR-код
+    qr = qrcode.make(text)
+    img_io = BytesIO()
+    qr.save(img_io)
+    img_io.seek(0)
+
+    return Response(content=img_io.read(), media_type="image/png")
+
+
 @get("/live_status")
 async def hello_world() -> str:
     return "Hello, world!"
 
 
 app = Litestar(
-    [hello_world, home],
+    [hello_world, home, encode],
     openapi_config=OpenAPIConfig(
         title="My API",
         description="This is the description of my API",
