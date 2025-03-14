3b3df8aac251507cd863ce73bbcad3411b7420d9
nikmosi <i@nikmosi.ru>
2025-03-14 22:16:27 +0700

    extract encode

--- app/app.py
+++ app/app.py
@@ -1,8 +1,6 @@
-from io import BytesIO
 from pathlib import Path
 
-import qrcode
-from litestar import Litestar, Request, Response, get, post
+from litestar import Litestar, get
 from litestar.contrib.jinja import JinjaTemplateEngine
 from litestar.openapi.config import OpenAPIConfig
 from litestar.openapi.plugins import SwaggerRenderPlugin
@@ -15,32 +13,13 @@ def home() -> Template:
     return Template(template_name="index.html")
 
 
-@post("/encode")
-async def encode(request: Request) -> Response:
-    form_data = await request.form()
-    text = form_data.get("data", "")
-
-    if not text:
-        return Response(
-            content="No data provided", media_type="text/plain", status_code=400
-        )
-
-    # Генерируем QR-код
-    qr = qrcode.make(text)
-    img_io = BytesIO()
-    qr.save(img_io)
-    img_io.seek(0)
-
-    return Response(content=img_io.read(), media_type="image/png")
-
-
 @get("/live_status")
 async def hello_world() -> str:
     return "Hello, world!"
 
 
 app = Litestar(
-    [hello_world, home, encode],
+    [hello_world, home],
     openapi_config=OpenAPIConfig(
         title="My API",
         description="This is the description of my API",
